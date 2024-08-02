5811ec359fca217272c8c24f6b4d76e5a6cf7fe3
alessandro.lapini <alessandro.lapini@conjurer.es>
2024-08-02 14:56:41 +0200

    add modal

--- src/players/components/PlayerTools.tsx
+++ src/players/components/PlayerTools.tsx
@@ -1,7 +1,16 @@
 import React, { useState } from 'react'
+import { IoPersonAddSharp } from "react-icons/io5";
+import AddPlayerModal from './AddPlayerModal';
 
 export default function PlayerTools() {
     const [isActive, setIsActive] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
+
+    const handleSearch = (e) => {
+
+    }
+
+    console.log(openModal)
 
     return (
         <div>
@@ -12,16 +21,31 @@ export default function PlayerTools() {
             </label>
             {isActive ?
                 <div className=''>
-                    <form className="max-w-md mx-auto">
+                    <form className="flex flex-wrap gap-5 max-w-md mx-auto items-center text-center justify-center">
+                    <div className='flex flex-wrap items-center gap-2 p-4 cursor-pointer text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 hover:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>
+                        <IoPersonAddSharp />
+                        <input  onClick={() => setOpenModal(!openModal)} type="button" className='cursor-pointer' value="Add player" />
+                    </div>
+                    <AddPlayerModal openModal={openModal} />
                         <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                         <div className="relative">
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                                 <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                                 </svg>
                             </div>
-                            <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-                            <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                            <input 
+                                type="search" 
+                                id="default-search" 
+                                className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Search players" 
+                                required />
+                            <button 
+                                type="submit"
+                                className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                >
+                                Search
+                            </button>
                         </div>
                     </form>
                 </div>
